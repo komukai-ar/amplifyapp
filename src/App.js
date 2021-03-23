b6b9c3a630552df55078072068fde83f1cceb7b5
小向 有 <komukai-a@gnavi.co.jp>
2021-03-23 19:13:25 +0900

    APIとDBを追加

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
-const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Reactで実装してみたぞ！！</h1>
-      </header>
-      <AmplifySignOut />
-    </div>
-)
+const initialFormState = { name: '', description: '' }
+
+const App = () => {
+    const [notes, setNotes] = useState([]);
+    const [formData, setFormData] = useState(initialFormState);
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    /**
+     * ノートを取得
+     * @return {Promise<void>}
+     */
+    const fetchNotes = async() => {
+        const apiData = await API.graphql({ query: listNotes });
+        setNotes(apiData.data.listNotes.items);
+    }
+
+    /**
+     * ノートを作成
+     * @return {Promise<void>}
+     */
+    const createNote = async() => {
+        if (!formData.name || !formData.description) return;
+        await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+        setNotes([ ...notes, formData ]);
+        setFormData(initialFormState);
+    }
+
+    /**
+     * ノートを削除
+     * @param id
+     * @return {Promise<void>}
+     */
+    const deleteNote = async({ id }) => {
+        const newNotesArray = notes.filter(note => note.id !== id);
+        setNotes(newNotesArray);
+        await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+    }
+
+    return (
+        <div className="App">
+            <h1>My Notes App</h1>
+            <input
+                onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+                placeholder="ノート名"
+                value={formData.name}
+            />
+            <input
+                onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+                placeholder="内容"
+                value={formData.description}
+            />
+            <button onClick={createNote}>Create Note</button>
+            <div style={{marginBottom: 30}}>
+                {
+                    notes.map(note => (
+                        <div key={note.id || note.name}>
+                            <h2>{note.name}</h2>
+                            <p>{note.description}</p>
+                            <button onClick={() => deleteNote(note)}>Delete note</button>
+                        </div>
+                    ))
+                }
+            </div>
+            <AmplifySignOut />
+        </div>
+    );
+}
 
 export default withAuthenticator(App);
